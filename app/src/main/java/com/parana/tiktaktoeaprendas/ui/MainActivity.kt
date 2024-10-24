776a825a7a9c7e9d5f7bf577cdd342e4ed538e4b
Eduardo Barcelos Viana <160142811+eduardob1503@users.noreply.github.com>
2024-10-24 16:36:50 -0300

    criação do banco

--- app/src/main/java/com/parana/tiktaktoeaprendas/ui/MainActivity.kt
+++ app/src/main/java/com/parana/tiktaktoeaprendas/ui/MainActivity.kt
@@ -2,6 +2,7 @@ package com.parana.tiktaktoeaprendas.ui
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.parana.tiktaktoeaprendas.R
@@ -40,12 +41,20 @@ class MainActivity : AppCompatActivity() {
         btSair.setOnClickListener {
             finish()
         }
-
+//Método para popular os jogadores da DataBase
         populaDataBaseUser()
+
+
     }
 
     private fun populaDataBaseUser() {
         try {
+
+//            bancoDados.readableDatabase.execSQL(
+//                "SELECT COUNT id FROM alunos;"
+//            )
+
+
             bancoDados.writableDatabase.execSQL(
                 "INSERT INTO alunos VALUES(null, 'Gustavo', 0);"
             )
@@ -68,5 +77,7 @@ class MainActivity : AppCompatActivity() {
             e.printStackTrace()
         }
     }
+
+
 
 }
