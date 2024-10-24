7096af86b177c905205f1e077c614626ebc51fd0
mrlmuriloWS <margonarmurilo@gmail.com>
2024-10-24 14:09:26 -0300

    [FEATURE] Botão de sair

--- app/src/main/java/com/parana/tiktaktoeaprendas/MainActivity.kt
+++ app/src/main/java/com/parana/tiktaktoeaprendas/MainActivity.kt
@@ -1,20 +1,23 @@
 package com.parana.tiktaktoeaprendas
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btSair: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        btSair = findViewById(R.id.bt_sair)
+        btSair.setOnClickListener{
+            finish()
+        }   
     }
 }
