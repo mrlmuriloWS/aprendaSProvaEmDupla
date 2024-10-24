ff7eb5140c5fe980f9a51b4edc7542f01815d4a7
mrlmuriloWS <margonarmurilo@gmail.com>
2024-10-24 15:10:30 -0300

    [feature] tratativa caso seja player vs cpu

--- app/src/main/java/com/parana/tiktaktoeaprendas/ui/PlayerChooserScreen.kt
+++ app/src/main/java/com/parana/tiktaktoeaprendas/ui/PlayerChooserScreen.kt
@@ -1,6 +1,10 @@
 package com.parana.tiktaktoeaprendas.ui
 
 import android.os.Bundle
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.ListAdapter
+import android.widget.Spinner
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,10 +13,45 @@ import com.parana.tiktaktoeaprendas.R
 
 class PlayerChooserScreen : AppCompatActivity() {
 
+    private lateinit var spinner1: Spinner
+    private lateinit var spinner2: Spinner
+    private var cpu = false;
+
+    private var listaPlayers = mutableListOf("Escolha um jogador")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_player_chooser_screen)
+
+
+//        if (intent.extras?.getBoolean("cpu") != null) {
+
+            cpu = intent.extras!!.getBoolean("cpu")
+//        }
+        spinner1 = findViewById(R.id.sp_player1)
+        spinner2 = findViewById(R.id.sp_player2)
+        popularSpinners()
+    }
+
+    fun popularSpinners() {
+        //(Esperando ficar pronto o banco para fazer os inserts dos itens)
+
+        //Caso seja player x cpu ele remove o botão
+
+        spinner1.adapter = ArrayAdapter(
+            this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,
+            listaPlayers
+        )
+
+        //Configura spinner para caso seja CPU
+        if (cpu) {
+            spinner2.visibility = View.GONE
+        } else {
+            spinner2.adapter = ArrayAdapter(
+                this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,
+                listaPlayers
+            )
+        }
     }
 
 }
